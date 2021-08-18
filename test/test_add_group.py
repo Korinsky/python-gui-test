842d46c11b04f791aded272df99eb6b65b7a4531
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-08-18 12:55:54 +0300

    29- генерация отчётов Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,13 @@
+import pytest
+
+
 def test_add_group(app):
-    old_list = app.groups.get_group_list()
-    app.groups.add_new_group("my group")
-    new_list = app.groups.get_group_list()
-    old_list.append("my group")
-    print(old_list)
-    assert sorted(old_list) == sorted(new_list)
+    with pytest.allure.step("Given a group list"):
+        old_list = app.groups.get_group_list()
+    with pytest.allure.step("When I add the group to the list"):
+        app.groups.add_new_group("my group")
+    with pytest.allure.step("Then the new group list is equal to the old list with the added group"):
+        new_list = app.groups.get_group_list()
+        old_list.append("my group")
+        print(old_list)
+        assert sorted(old_list) == sorted(new_list)
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,12 +1,17 @@
 import random
+import pytest
+
 
 def test_delete_group(app):
-    old_list = app.groups.get_group_list()
-    if len(old_list) == 1:
-        app.groups.add_new_group("my group")
+    with pytest.allure.step("Given a group list"):
         old_list = app.groups.get_group_list()
-    index = old_list.index(random.choice(old_list))
-    app.groups.delete_group_by_index(index)
-    new_list = app.groups.get_group_list()
-    old_list.remove(old_list[index])
-    assert sorted(old_list) == sorted(new_list)
+        if len(old_list) == 1:
+            app.groups.add_new_group("my group")
+            old_list = app.groups.get_group_list()
+    with pytest.allure.step("When I deleted the group to the list"):
+        index = old_list.index(random.choice(old_list))
+        app.groups.delete_group_by_index(index)
+    with pytest.allure.step("Then the new group list is equal to the old list without deleted group"):
+        new_list = app.groups.get_group_list()
+        old_list.remove(old_list[index])
+        assert sorted(old_list) == sorted(new_list)
